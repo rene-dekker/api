1fb866b240bafbb8c6ed20aac0e17d268e9abd20
Neil Jerram <neil@tigera.io>
2021-11-15 10:19:19 +0000

    Add runtime-security as target data set for GlobalAlert

--- pkg/apis/projectcalico/v3/globalalert.go
+++ pkg/apis/projectcalico/v3/globalalert.go
@@ -15,6 +15,7 @@ const (
 	GlobalAlertDataSetAudit = "audit"
 	GlobalAlertDataSetDNS   = "dns"
 	GlobalAlertDataSetFlows = "flows"
+	GlobalAlertDataSetRS    = "rs"
 
 	GlobalAlertMetricAvg   = "avg"
 	GlobalAlertMetricMax   = "max"
@@ -45,7 +46,7 @@ type GlobalAlertSpec struct {
 	Severity    int              `json:"severity" validate:"required,min=1,max=100"`
 	Period      *metav1.Duration `json:"period,omitempty" validate:"omitempty"`
 	Lookback    *metav1.Duration `json:"lookback,omitempty" validate:"omitempty"`
-	DataSet     string           `json:"dataSet" validate:"required,oneof=flows dns audit"`
+	DataSet     string           `json:"dataSet" validate:"required,oneof=flows dns audit rs"`
 	Query       string           `json:"query,omitempty" validate:"omitempty"`
 	AggregateBy []string         `json:"aggregateBy,omitempty" validate:"omitempty"`
 	Field       string           `json:"field,omitempty" validate:"omitempty"`
